2292fb407fd723f9fd175ecb225ebb2811216afa
Dangornushi <dangomushi@Yuma.local>
2023-03-27 18:31:03 +0900

    ADD: コマンドの雛形作成

--- src/main/java/dev/shiro8613/missionplugin/MissionPlugin.java
+++ src/main/java/dev/shiro8613/missionplugin/MissionPlugin.java
@@ -38,12 +38,40 @@ public final class MissionPlugin extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        /*
+            /mission start (Number) //ミッション開始
+            /mission list //ミッション一覧表示
+            /mission forcestop //ミッション強制停止
+            /mission state //ミッション実行中かどうか
+        */
+
         sender.sendMessage("実行するコマンドは〜〜〜〜？！？！？！？！ " + args[0]);
         String className = missionManager.getMissionNames()[Integer.parseInt(args[0])-1];
         sender.sendMessage("実行できるクラス名は〜〜〜〜？！？！？！？！ " + className);
 
-        // ミッション1を実行!
-        missionManager.startMission(className);
+        switch (args[1]) {
+            case "start": {
+                // ミッションを実行!
+                missionManager.startMission(className);
+                break;
+            }
+
+            case "list": {
+                // ミッション一覧
+                break;
+            }
+
+            case "forcestop": {
+                // ミッション強制停止
+                break;
+            }
+
+            case "state": {
+                // ミッション実行中かどうか
+                break;
+            }
+        }
+
         return true;
     }
 }
