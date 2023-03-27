52735473df68cccb12226da274937a2952e8451d
toms0910 <psp1070@gmail.com>
2023-03-27 20:36:08 +0900

    getPlayersをじっそうしたよ

--- src/main/java/dev/shiro8613/missionplugin/mission/Mission.java
+++ src/main/java/dev/shiro8613/missionplugin/mission/Mission.java
@@ -3,8 +3,13 @@ package dev.shiro8613.missionplugin.mission;
 import dev.shiro8613.missionplugin.event.EventManager;
 import dev.shiro8613.missionplugin.utils.timer.TimerManager;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Mission {
 
@@ -21,9 +26,19 @@ public abstract class Mission {
         return timerManager;
     }
 
+    public List<Player> getPlayers() {
+        List<Player> players = new ArrayList<>();
+        List<World> worlds = getJavaPlugin().getServer().getWorlds();
+        worlds.forEach(world -> {
+            players.addAll(world.getPlayers());
+        });
+
+        return players;
+    }
 
     public void missionEnd() {
         missionManager.getProgressMission().cancel();
+        eventManager.removeAll();
     }
 
     public EventManager getEventManager() {
