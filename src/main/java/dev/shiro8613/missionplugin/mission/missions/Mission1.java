f290f36f3d69b115884f94cebbdb478d289cecf5
toms0910 <psp1070@gmail.com>
2023-03-29 22:57:13 +0900

    無効化された時に呼ばれる関数ができたにゃー－－－－ん

--- src/main/java/dev/shiro8613/missionplugin/mission/missions/Mission1.java
+++ src/main/java/dev/shiro8613/missionplugin/mission/missions/Mission1.java
@@ -1,24 +1,43 @@
 package dev.shiro8613.missionplugin.mission.missions;
 
-import dev.shiro8613.missionplugin.event.EventEnum;
 import dev.shiro8613.missionplugin.mission.Mission;
-import dev.shiro8613.missionplugin.utils.event.EventUtil;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+
 
 
 public class Mission1 extends Mission {
 
+    private final BossBar bossBar = Bukkit.createBossBar("残り時間:", BarColor.YELLOW, BarStyle.SOLID);
+    private int tick = 0;
+    private int sec;
+    private int tmpSec;
+    private double view;
     @Override
     public void Init() {
-        getEventManager().registerEventHandler(EventEnum.ClickEvent, (event) -> {
-            PlayerInteractEvent ev = EventUtil.convertEvent(EventEnum.ClickEvent, event ); //これでeventが別な型にキャストされるよ！
-
-        });
+        bossBar.setVisible(true);
+        sec = 30;
+        view = 1;
+        tmpSec = sec % 100;
     }
 
     @Override
     public void Tick() {
+        getPlayers().forEach(bossBar::addPlayer);
+        if(sec <= 0) { bossBar.removeAll(); missionEnd(); return; }
+        if((tick % 20 == 0)) { sec --; tick = 0;}
+        if((tick % 4) == 0) view -= tmpSec;
+        bossBar.setTitle("残り時間: " + sec + "秒");
+        bossBar.setProgress(view);
+        tick++;
 
+    }
+
+    @Override
+    public void onDisable() {
+        bossBar.removeAll();
     }
 
 }
