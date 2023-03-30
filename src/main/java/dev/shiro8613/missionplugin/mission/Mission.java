a3eeb81af57a9929b2f540f02a70feae30f80f12
toms0910 <psp1070@gmail.com>
2023-03-30 20:44:51 +0900

    よけいなものけした

--- src/main/java/dev/shiro8613/missionplugin/mission/Mission.java
+++ src/main/java/dev/shiro8613/missionplugin/mission/Mission.java
@@ -1,14 +1,12 @@
 package dev.shiro8613.missionplugin.mission;
 
-import dev.shiro8613.missionplugin.command.Command;
 import dev.shiro8613.missionplugin.command.CommandManager;
 import dev.shiro8613.missionplugin.event.EventManager;
 import dev.shiro8613.missionplugin.utils.timer.TimerManager;
-import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,9 +34,7 @@ public abstract class Mission {
     public List<Player> getPlayers() {
         List<Player> players = new ArrayList<>();
         List<World> worlds = getJavaPlugin().getServer().getWorlds();
-        worlds.forEach(world -> {
-            players.addAll(world.getPlayers());
-        });
+        worlds.forEach(world -> players.addAll(world.getPlayers()));
 
         return players;
     }
