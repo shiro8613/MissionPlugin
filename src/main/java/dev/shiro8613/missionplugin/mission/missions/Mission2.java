69b98652420a3dc5e23b78c2df08b4bea841a2a5
yahho <minecraftgasukinafumi@gmail.com>
2023-03-31 00:13:19 +0900

    &Sweep!
    Signed-off-by: yahho <minecraftgasukinafumi@gmail.com>

--- src/main/java/dev/shiro8613/missionplugin/mission/missions/Mission2.java
+++ src/main/java/dev/shiro8613/missionplugin/mission/missions/Mission2.java
@@ -24,24 +24,15 @@ import java.util.List;
 
 public class Mission2 extends Mission{
 
-//    private Player player = null;
     private List<Player> challengers = null;
     private final ItemStack reward = new ItemStack(Material.AIR);
-//    private final BossBar bar = Bukkit.createBossBar("残り時間:", BarColor.YELLOW, BarStyle.SOLID);
     private int state;
-//    private int tickCounter;
-    //private int sixMin = 180*2;
     private int timeLimit = 6*Timer.TICKS_1_MIN;
     private int nowPhase = 0;
 
     @Override
     public void Init() {
-//        player = getPlayers().get(0);
         challengers = getPlayers().stream().filter(p -> p.getScoreboard().getTeam("challenger") != null).toList();
-//        player.sendTitle("ミッション発動\n迷子のお知らせ","Subtitle",10,70,20);
-//        player.getWorld().playSound(player.getLocation(), org.bukkit.Sound.ENTITY_PLAYER_LEVELUP,1,1);
-//        bossBar = 10;
-        //480
 
         greet();
         // 雑に作ったTimerを使ってみる
@@ -50,35 +41,12 @@ public class Mission2 extends Mission{
         getTimerManager().getTimerByName("mission.2.find_shiratama").setSubscribers(challengers);
         getTimerManager().getTimerByName("mission.2.find_shiratama").setVisibility(true);
 
-//        tickCounter = sixMin;
-//        player.sendMessage(
-//                """
-//                        迷子のお知らせです。
-//                        ピンクのスカートを着て、サングラス を掛けた、白玉のような女の子を探しています。もし見つけましたらその子と一緒 に写真を撮り、写真をdiscordの『#逃走中ミッション』チャンネルにお貼りください。
-//                        5人の逃走者が写真を貼ることができればミッション達成です。
-//                        全員に俊足の ポーションをお渡しします。
-//                        もし制限時間内に達成できなければ、逃走者全員が10 秒間発光します。""");
     }
 
     @Override
     public void Tick() {
 
         getTimerManager().getTimerByName("mission.2.find_shiratama").tickTimer();
-//        if (tickCounter == sixMin && bossBar >= 0) {
-//            //getJavaPlugin().getServer().sendMessage(Component.text(bossBar*0.1));
-//            bar.setProgress(bossBar--*0.1);
-//            bar.addPlayer(player);
-//            bar.setVisible(true);
-//            tickCounter = 0;
-//            if (bossBar == 0) {
-//                givePotion();
-//                bossBar = -1;
-//            }
-//
-//        }
-//        else if (tickCounter < sixMin) {
-//            tickCounter++;
-//        }
 
         if (getTimerManager().getTimerByName("mission.2.find_shiratama").isFinished()) {
             givePotion();
@@ -86,7 +54,6 @@ public class Mission2 extends Mission{
         }
 
         if (state == -1) {
-//            bar.removeAll();
             challengers.forEach(p ->p.sendMessage("ミッションを終了します"));
             // ここのメソッド動かない！！
             // そんな時代も414fdac919775820ce5dc2582d04c2b39dd34be1より前にはありました...
@@ -150,7 +117,6 @@ public class Mission2 extends Mission{
 
     @Override
     public void onDisable() {
-//        bar.removeAll();
         getTimerManager().discardTimerByName("mission.2.find_shiratama");
     }
 }
