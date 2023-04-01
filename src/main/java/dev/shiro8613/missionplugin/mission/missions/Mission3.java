3878120d4873de446a96d159b2f66a5ff6229be9
Dangornushi <dangomushi@Yuma.local>
2023-04-01 21:28:03 +0900

    ADD: ボタンを押したら消える処理を追加：凹んでる最中にもう一回押さないと反映されない

--- src/main/java/dev/shiro8613/missionplugin/mission/missions/Mission3.java
+++ src/main/java/dev/shiro8613/missionplugin/mission/missions/Mission3.java
@@ -12,8 +12,11 @@ import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.FaceAttachable;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
@@ -21,6 +24,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.material.Button;
+import org.bukkit.material.MaterialData;
 import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -35,7 +40,9 @@ import static org.bukkit.Bukkit.getServer;
 public class Mission3 extends Mission {
     private Player player = null;
     private List<Player> nonHunters = null;
+
     private enum MissionState {Start, OnGoing, End}
+
     private final BossBar bar = Bukkit.createBossBar("残り時間:", BarColor.YELLOW, BarStyle.SOLID);
     private final ItemStack reward = new ItemStack(Material.AIR);
     private int timeLimit;
@@ -43,25 +50,27 @@ public class Mission3 extends Mission {
     private MissionState state = MissionState.Start;
     private final int requiredChecks = 5;
     private Location buttonLocation;
+
     @Override
     public void Init() {
         challengers = getPlayers().stream().filter(p -> p.getScoreboard().getTeam("challenger") != null).toList();
         nonHunters = getPlayers().stream().filter(p -> p.getScoreboard().getTeam("hunter") == null).toList();
         state = MissionState.OnGoing;
-        player = getPlayers().get(0); timeLimit = 5*Timer.TICKS_1_MIN;
+        player = getPlayers().get(0);
+        timeLimit = 5 * Timer.TICKS_1_MIN;
         buttonLocation = new Location(getServer().getWorld("world"), 0, 63, 11);
         spawnStoneButton(buttonLocation);
-        buttonLocation.setY(buttonLocation.getY()+1);
+        buttonLocation.setY(buttonLocation.getY() + 1);
 
         getEventManager().registerEventHandler(EventEnum.ClickEvent, eventContext -> {
             PlayerInteractEvent playerInteractEvent = eventContext.getEvent(EventEnum.ClickEvent);
             Block block = playerInteractEvent.getClickedBlock();
-            if(Objects.requireNonNull(block).getType().equals(Material.STONE_BUTTON)) {
-                Location location = block.getLocation();
+            if (Objects.requireNonNull(block).getType().equals(Material.STONE_BUTTON)) {
                 //1,1,1は座標
-                if(location.equals(buttonLocation)) {
+                if (block.getLocation().equals(buttonLocation)) {
                     //処理
-                    player.sendMessage("OK");
+                    // 石のボタンを消す処置：ボタンが凹んでる時にもう一回押さないと反映しない
+                    removeStoneButton(buttonLocation);
                 }
             }
         });
@@ -73,6 +82,7 @@ public class Mission3 extends Mission {
         getTimerManager().getTimerByName("mission.3.push_button").setSubscribers(nonHunters);
         getTimerManager().getTimerByName("mission.3.push_button").setVisibility(true);
     }
+
     @Override
     public void Tick() {
 
@@ -83,7 +93,7 @@ public class Mission3 extends Mission {
             state = MissionState.End;
             final var failTitle = Component.text("ミッション失敗", NamedTextColor.RED);
             final var failSubTitle = Component.text("ミッションに失敗したため、逃走者全員に発光エフェクトが付与されました。", NamedTextColor.GOLD, TextDecoration.ITALIC);
-            var deBuff = new PotionEffect(PotionEffectType.GLOWING, Timer.TICKS_1_SEC*10, 1);
+            var deBuff = new PotionEffect(PotionEffectType.GLOWING, Timer.TICKS_1_SEC * 10, 1);
             challengers.forEach(p -> {
                 p.addPotionEffect(deBuff);
                 p.showTitle(Title.title(failTitle, failSubTitle));
@@ -95,6 +105,7 @@ public class Mission3 extends Mission {
             missionEnd();
         }
     }
+
     @Override
     public void onDisable() {
         getTimerManager().discardTimerByName("mission.3.push_button");
@@ -109,13 +120,13 @@ public class Mission3 extends Mission {
         var pm = (PotionMeta) reward.getItemMeta();
         pm.setBasePotionData(new PotionData(PotionType.WATER));
         pm.displayName(Component.translatable("item.minecraft.potion.effect.swiftness").color(NamedTextColor.AQUA));
-        pm.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, 10*Timer.TICKS_1_SEC,3, false, true, true), true);
+        pm.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, 10 * Timer.TICKS_1_SEC, 3, false, true, true), true);
         reward.setItemMeta(pm);
 
         var title = Component.text("ミッション発動: ボタンを押せ！", NamedTextColor.YELLOW);
         var subTitle = Component.text("詳細はチャットを確認してください", NamedTextColor.GRAY, TextDecoration.ITALIC);
 
-        nonHunters.forEach( p -> {
+        nonHunters.forEach(p -> {
             p.showTitle(Title.title(title, subTitle));
             p.playSound(Sound.sound(org.bukkit.Sound.ENTITY_PLAYER_LEVELUP, Sound.Source.PLAYER, 1.0f, 1.0f));
             p.sendMessage(Component.text("おいおい、さっきからずっと隠れてる人がいるんじゃないか？逃走中なのにけしからん！ショッピングモール内に石のボタン（ダイヤモンド ブロックに設置されている）を10個置いた。制限時間は5分。全部おせなきゃ全員発光！ミッションを達成してもボタンを押さなかった人は発光するぞ！ボタンを押して、ミッションも達成できたら特殊アイテムゲットだ！", NamedTextColor.YELLOW));
@@ -124,15 +135,15 @@ public class Mission3 extends Mission {
 
     // 石のボタンを座標指定で出現させる関数
     public void spawnStoneButton(Location location) {
-        Block block = location.getBlock();
-        Block buttonBlock = block.getRelative(BlockFace.UP);
-        buttonBlock.setType(Material.STONE_BUTTON);
+        Block buttonBlock = location.getBlock().getRelative(BlockFace.UP); // 上向きの石のボタンを作成するためにブロックの上の位置を取得する
+        buttonBlock.setType(Material.STONE_BUTTON); // ブロックを石のボタンに設定
     }
 
     // 石のボタンを座標指定で消す関数
     public void removeStoneButton(Location location) {
-        Block block = location.getBlock();
-        Block buttonBlock = block.getRelative(BlockFace.UP);
-        buttonBlock.setType(Material.AIR);
+        Block block = location.getWorld().getBlockAt(location);
+        block.setType(Material.AIR);
+        player.sendMessage("ボタンを消去!");
     }
+
 }
